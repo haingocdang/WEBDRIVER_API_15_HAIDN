deaac33b9d8195d10932816c6775dc629885b74d
HD <HD@192.168.1.8>
2020-01-01 16:01:31 +0700

    Commit project at home

---
+++ selenium/webdriver_api/Topic_00_Template.java
@@ -0,0 +1,51 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_00_Template {
+	// Khai bao 1 bien driver dai dien cho Selenium Driver
+	WebDriver driver;
+
+	//Pre-Condition
+	@BeforeClass
+	public void beforeClass() {
+		// Khoi tao FF
+		driver = new FirefoxDriver();
+		
+		// Cho cho element dc hien thi truoc khi tuong tac trong 30s
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		//Maximize browser
+		driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void TC_01_() {
+		
+	}
+
+	@Test
+	public void TC_02_() {
+		
+	}
+
+	@Test
+	public void TC_03_() {
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
---
+++ selenium/webdriver_api/Topic_01_CheckEnvironment.java
@@ -0,0 +1,65 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_CheckEnvironment {
+	// Khai bao 1 bien driver dai dien cho Selenium Driver
+	WebDriver driver;
+
+	//Pre-Condition
+	@BeforeClass
+	public void beforeClass() {
+		// Khoi tao FF
+		driver = new FirefoxDriver();
+		
+		// Cho cho element dc hien thi truoc khi tuong tac trong 30s
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		//Maximize browser
+		driver.manage().window().maximize();
+		
+		//Mo ra 1 trang web (AUT)
+		driver.get("http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		// Login Page Url matching
+		String loginPageUrl = driver.getCurrentUrl();
+		
+		// In ra console cho user thay dc result test
+		System.out.println(loginPageUrl);
+		
+		// Cac ham de verify data cua TestNG(true/false/ equal)
+		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+		// Login Page title
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+	}
+
+	@Test
+	public void TC_03_LoginFormDisplayed() {
+		// Login form displayed
+		
+		//Verify Login form display on Login page
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}

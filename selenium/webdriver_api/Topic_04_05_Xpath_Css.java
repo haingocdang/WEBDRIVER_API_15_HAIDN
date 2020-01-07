e14dce58d520f50ee9c5939172c64d9496579fb2
haingocdang <china3pleiku@gmail.com>
2020-01-07 10:45:19 +0700

    Remove Http.assert

--- selenium/webdriver_api/Topic_04_05_Xpath_Css.java
+++ selenium/webdriver_api/Topic_04_05_Xpath_Css.java
@@ -3,7 +3,7 @@ package webdriver_api;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.http.util.Asserts;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;

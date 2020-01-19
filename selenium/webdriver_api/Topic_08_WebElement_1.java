83d57e3c87485f4454cd456e24447280af4b131a
haingocdang <china3pleiku@gmail.com>
2020-01-19 15:34:08 +0700

    Commit isSelected, Is Enable Topic

---
+++ selenium/webdriver_api/Topic_08_WebElement_1.java
@@ -0,0 +1,239 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Topic_08_WebElement_1 {
+	// Khai bao 1 bien driver dai dien cho Selenium Driver
+	WebDriver driver;
+	By radioUnder18= By.id("under_18");
+	By email= By.id("mail");
+	By education= By.id("edu");
+	By passwordBy=By.id("password");
+	By jobRole2SelectBoxBy=By.xpath("//select[@id='job2']");
+	By developmentCheckboxBy=By.xpath("//input[@value='interest_development']");
+	
+	//Pre-Condition
+	@BeforeClass
+	public void beforeClass() {
+		// Khoi tao FF
+		driver = new FirefoxDriver();
+					
+		// Cho cho element dc hien thi truoc khi tuong tac trong 30s
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		//Maximize browser
+		driver.manage().window().maximize();
+		
+	}
+	
+	@BeforeMethod
+	public void beforemethod() {
+		// TODO Auto-generated method stub
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+	}
+
+//	@Test
+	public void TC_Check_isDisplay() throws InterruptedException {
+	//	WebElement emailTextbox=driver.findElement(email);
+	//	WebElement educationTextarea=driver.findElement(education);
+	//	WebElement radio18=driver.findElement(radioUnder18);
+		String data="Automation Testing";
+		
+		/*Assert.assertTrue(emailTextbox.isDisplayed());
+		Assert.assertTrue(radio18.isDisplayed());
+		Assert.assertTrue(educationTextarea.isDisplayed());
+		
+		emailTextbox.sendKeys(data);
+		Assert.assertEquals(emailTextbox.getText(), data);
+		System.out.println(emailTextbox.getText());
+		
+		educationTextarea.sendKeys(data);
+		Assert.assertEquals(educationTextarea.getText(), data);
+		System.out.println(educationTextarea.getText());
+		
+		radio18.click();
+		Assert.assertTrue(radio18.isSelected());*/
+	/*	if (emailTextbox.isDisplayed()) {
+			emailTextbox.sendKeys(data);
+			
+		}
+		
+		if (educationTextarea.isDisplayed()) {
+			educationTextarea.sendKeys(data);
+		}
+		
+		if (radio18.isDisplayed()) {
+			radio18.click();
+			
+		}
+	}*/
+		if (isElementDisplay(email)) {
+			sendkeyToElement(email, data);
+		}
+	}
+
+	// @Test
+	public void TC_02_Check_Enable() {
+		
+/*		WebElement emailTextbox=driver.findElement(email);
+		WebElement radioAge18=driver.findElement(radioUnder18);
+		WebElement educationTextarea=driver.findElement(education);
+		WebElement job1Selectbox=driver.findElement(By.xpath("//select[@id='job1']"));
+		WebElement developmentCheckbox=driver.findElement(By.xpath("//input[@value='interest_development']"));
+		WebElement slider1=driver.findElement(By.xpath("//input[@id='slider-1']"));
+		
+		if (emailTextbox.isEnabled()) {
+			System.out.println( "Element is enabled");
+		}
+
+		if (radioAge18.isEnabled()) {
+			System.out.println("Element is enabled");
+		}
+		
+		if (educationTextarea.isEnabled()) {
+			System.out.println("Element is enabled");
+		}
+		
+		if (job1Selectbox.isEnabled()) {
+			System.out.println("Element is enabled");
+		}
+		
+		if (developmentCheckbox.isEnabled()) {
+			System.out.println("Element is enabled");
+		}
+		
+		if (slider1.isEnabled()){
+			System.out.println("Element is enabled");
+		}
+		
+		WebElement passwordTextbox=driver.findElement(By.id("password"));
+		WebElement radioAgeDisable=driver.findElement(By.xpath("//input[@id='radio-disabled']"));
+		WebElement bioTextarea=driver.findElement(By.id("bio"));
+		WebElement jobRole2Textbox=driver.findElement(By.xpath("//select[@id='job2']/option[@value='automation']"));
+		WebElement checkboxDisable=driver.findElement(By.id("check-disbaled"));
+		WebElement slider2=driver.findElement(By.id("slider-2"));
+		
+		if (passwordTextbox.isEnabled()) {
+			System.out.println("Element is enabled");
+			
+		} else {
+			System.out.println("Element is disabled");
+
+		}
+						
+		if (radioAgeDisable.isEnabled()) {
+			System.out.println("Element is enabled");
+			
+		} else {
+			System.out.println("Element is disabled");
+
+		}
+		
+		if (bioTextarea.isEnabled()) {
+			System.out.println("Element is enabled");
+			
+		} else {
+			System.out.println("Element is disabled");
+
+		}
+		
+		if (jobRole2Textbox.isEnabled()) {
+			System.out.println("Element is enabled");
+			
+		} else {
+			System.out.println("Element is disabled");
+
+		}
+		
+		if (checkboxDisable.isEnabled()) {
+			System.out.println("Element is enabled");
+			
+		} else {
+			System.out.println("Element is disabled");
+
+		}
+		
+		if (slider2.isEnabled()) {
+			System.out.println("Element is enabled");
+			
+		} else {
+			System.out.println("Elment is disabled");
+
+		}*/
+		Assert.assertTrue(ischeckElementEnable(email));
+		Assert.assertTrue(ischeckElementEnable(education));
+		Assert.assertTrue(ischeckElementEnable(radioUnder18));
+		Assert.assertFalse(ischeckElementEnable(passwordBy));
+		Assert.assertFalse(ischeckElementEnable(jobRole2SelectBoxBy));
+	}
+
+	@Test
+	public void TC_03_IsSelected() throws InterruptedException {
+		
+		driver.findElement(radioUnder18).click();
+//		Thread.sleep(10000);
+		driver.findElement(developmentCheckboxBy).click();
+		Assert.assertTrue(ischeckElementSelecte(radioUnder18));
+		Assert.assertTrue(driver.findElement(developmentCheckboxBy).isSelected());
+		
+		driver.findElement(radioUnder18).click();
+		driver.findElement(developmentCheckboxBy).click();
+		Assert.assertTrue(driver.findElement(radioUnder18).isSelected());
+		Assert.assertFalse(ischeckElementSelecte(developmentCheckboxBy));
+		
+	}
+	
+	public boolean isElementDisplay(By by) {
+		WebElement element=driver.findElement(by);
+		if (element.isDisplayed()) {
+			return true;			
+		} else {
+			return false;
+
+		}
+	}
+	
+	public boolean ischeckElementEnable (By by) {
+		WebElement element=driver.findElement(by);
+		if (element.isEnabled()) {
+			System.out.println("Element [" + by +"] is enabled");
+			return true;
+		} else {
+			System.out.println("Element [" + by +"] is disabled");
+			return false;
+		}
+	}
+	
+	public boolean ischeckElementSelecte (By by) {
+		WebElement element=driver.findElement(by);
+		if (element.isSelected()) {
+			System.out.println("Element [" + by +"] is selected");
+			return true;
+		} else {
+			System.out.println("Element [" + by +"] is deselected");
+			return false;
+		}
+	}
+	
+	public void sendkeyToElement(By by, String value) {
+		WebElement element=driver.findElement(by);
+		element.sendKeys(value);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}

8a9887e9cf5beab98fd9ddf0e9b83732fcbc1503
haingocdang <china3pleiku@gmail.com>
2020-02-21 09:39:31 +0700

    Commit

--- selenium/webdriver_api/Topic_14_User_Interaction.java
+++ selenium/webdriver_api/Topic_14_User_Interaction.java
@@ -164,7 +164,7 @@ public class Topic_14_User_Interaction {
 		//Setup Robot
 		java.awt.Robot robot ;
 		robot= new java.awt.Robot();
-		robot.setAutoDelay(70);
+		robot.setAutoDelay(90);
 		
 		//Get Size elements		
 		Dimension sourceSize=source.getSize();
